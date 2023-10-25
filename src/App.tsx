0f64b35b866c2949576fb4e063e04bae8459dc08
AlanMestezk <alanmestezk98@gmail.com>
2023-10-25 10:35:51 -0300

    Section 4: Atualização

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 
-import { useContext, useState } from 'react'
-import './App.css'
-import { Player } from './components/player'
+import { useContext, useState }          from 'react'
+import { Player }                        from './components/player'
 import { PlayerContext, PlayerProvider } from './context/player'
-import { Register } from './components/register'
+import { Register }                      from './components/register'
+import './App.css'
 
 function App() {
-
-  const {
-    name, person, kind, force, defense, agility
-  } = useContext(PlayerContext)
-
-  const [playerName, setPlayerName]       = useState(name)
-  const [playerPerson, setPlayerPerson]   = useState(person)
-  const [playerKind, setPlayerKind]       = useState(kind)
-  const [playerForce, setPlayerForce]     = useState<number | any>(force)
-  const [playerDefense, setPLayerDefence] = useState<string | number>(defense)
-  const [playerAgility, setPLayerAgility] = useState<string | number>(agility)
-
 
   const [showPlayer, setShowPlayer] = useState(true)
 
